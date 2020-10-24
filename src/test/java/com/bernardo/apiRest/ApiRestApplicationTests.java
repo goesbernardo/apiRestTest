6e16a6c20d0f665c649ded2701b3c428e0dd16f2
goesbernardo <bernardo.goes01@gmail.com>
2020-10-24 17:10:30 -0300

    firts commit

---
+++ src/test/java/com/bernardo/apiRest/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardo.apiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
